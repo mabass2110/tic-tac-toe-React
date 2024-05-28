cd923dbd8f14061dced0412fdf1ca121d0e52480
helder mabasso <hfmabasso@gmail.com>
2024-05-28 23:51:55 +0200

    added a Square component and a function named renderSquare

--- src/colorGame00.jsx
+++ src/colorGame00.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+
+const Square = () => {
+  return <button></button>
+}
 const Board = () => {
   const [player, setPlayer] = React.useState(1);
   let status = `Player ${player}`;
+
+  const renderSquare = () => {
+    return <Square></Square>
+  }
   return (
-    <div
-      className="game-board"
-      onClick={(e) => {
-        setPlayer((player + 1) % 2);
-        status = `Player ${player}`;
-      }}
-    >
+    <div className="game-board">
+
+      <div className="grid-row">
+        {renderSquare(0)}
+      </div>
       <div id="info">
         <h1>{status}</h1>
       </div>
