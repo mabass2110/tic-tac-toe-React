d194f445bfccdae048188e35818164d85386f191
helder mabasso <hfmabasso@gmail.com>
2024-05-28 23:36:10 +0200

    Starter files

---
+++ src/colorGame00.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+const Board = () => {
+  const [player, setPlayer] = React.useState(0);
+  let status = `Player ${player}`;
+  return (
+    <div
+      className="game-board"
+      onClick={(e) => {
+        setPlayer(player + 1);
+        status = `Player ${player}`;
+      }}
+    >
+      <div id="info">
+        <h1>{status}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Board;
